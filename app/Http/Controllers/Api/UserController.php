cd7abe63384f331123a113f8156f1ba2dbddaa21
Данил Мелихов <melihovdanil8@gmail.com>
2023-06-04 22:26:32 +0600

    fix empty avatar error, backend response lk front

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -19,24 +19,17 @@ class UserController extends Controller
      */
     public function createUser(Request $request)
     {
-
-        // return response()->json([
-        //     'status'=>true,
-        //     'data'=>'mydata',
-        //     'message'=>'new msg',
-        // ], 200);
         try {
             //Validated
             $validateUser = Validator::make($request->all(), 
             [
-                //'avatar' => 'required',
+                'avatar' => 'required',
                 'type' => 'required',
                 'open_id' => 'required',
                 'name' => 'required',
                 'email' => 'required',
                 //'password' => 'required|min:6',
             ]);
-
             if($validateUser->fails()){
                 return response()->json([
                     'status' => false,
@@ -52,11 +45,9 @@ class UserController extends Controller
             $map['type'] = $validated['type'];
             $map['open_id'] = $validated['open_id'];
             $user = User::where($map)->first();
-            return response()->json([
-                'status'=>true,
-                'data'=>'mydata',
-                'message'=>'new msg',
-            ], 200);
+
+            
+
             //whether user has already logged or not
             //empty means doesnt exist then save user in the db for first time
             if(empty($user->id)){
@@ -64,12 +55,18 @@ class UserController extends Controller
                 //out job assign user in db
                 //this token is user id
                 $validated['token'] = md5(uniqid().rand(10000, 99999));
+
+                
+
                 //user first time created
                 $validated['created_at']= Carbon::now();
                 //encrypt pass
                 //$validated['password']=Hash::make($validated['password']);
                 //returns the id of the row after saving
                 $userID = User::insertGetId($validated);
+
+                
+
                 //user's all information
                 $userInfo = User::where('id', '=', $userID)->first();
                 //
@@ -80,8 +77,8 @@ class UserController extends Controller
                 User::where('id', '=', $userID)->update(['access_token'=>$accessToken]);
 
                 return response()->json([
-                    'status' => true,
-                    'message' => 'User Created Successfully',
+                    'code' => 200,
+                    'msg' => 'User Created Successfully',
                     'data' => $userInfo
                     
                 ], 200);
@@ -90,17 +87,10 @@ class UserController extends Controller
             $accessToken = $user->createToken(uniqid())->plainTextToken;
             $user->access_token = $accessToken;
             User::where('open_id', '=', $validated['open_id'])->update(['access_token'=>$accessToken]);
-
-            // $user = User::create([
-            //     'name' => $request->name,
-            //     'email' => $request->email,
-            //     'password' => Hash::make($request->password)
-            // ]);
-
             return response()->json([
-                'status' => true,
-                'message' => 'User logged in Successfully',
-                'token' => $user
+                'code' => 200,
+                    'msg' => 'User Created Successfully',
+                    'data' => $user
             ], 200);
 
         } catch (\Throwable $th) {
