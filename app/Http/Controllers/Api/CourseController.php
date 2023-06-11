318a16ccf4d970b97689e4ecc19c541fac86fc65
Данил Мелихов <melihovdanil8@gmail.com>
2023-06-11 21:27:54 +0600

    error handling courseAPI added

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -9,13 +9,22 @@ use Illuminate\Http\Request;
 class CourseController extends Controller
 {
     //returning all course list
-    public function courseList(){
-        $result = Course::select('course_name', 'thumbnail', 'lesson_num', 'price', 'id')->get();
+    public function courseList()
+    {
+        try {
+            $result = Course::select('course_name', 'thumbnail', 'lesson_num', 'price', 'id')->get();
 
-        return response()->json([
-            'code' => 200,
-            'msg' => 'My course list is here',
-            'data' => $result
-        ], 200);
+            return response()->json([
+                'code' => 200,
+                'msg' => 'My course list is here',
+                'data' => $result
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'code' => 500,
+                'msg' => $th->getMessage(),
+                'data' => null,
+            ], 500);
+        }
     }
 }
